1d2df53f4832fea8163cc3484b86b1b8cf5424e4
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-12-30 00:41:20 +0530

    coming soon page ready

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import Aikiam from './aikiam.svg';
+import Coming_Soon from './coming_soon.svg';
+import Coming_Soon_lap from './coming_soon_lap.svg';
+import Kalaama from './kalaama.svg';
+import Kaloolsavm from './kaloolsavm.svg';
+import our_logo from './our_logo.svg';
+import date from './date.svg';
+
+export { Aikiam, Coming_Soon, Kalaama, Kaloolsavm, our_logo, Coming_Soon_lap, date };
