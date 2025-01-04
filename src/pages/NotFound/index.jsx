df2c2954fa3c5d3c0cc2687272e727aa4dc80e2a
muhammedshamil8 <muhammedshamil008@gmail.com>
2025-01-04 22:00:49 +0530

    feat: initialize project with base structure and configurations

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+    </div>
+  )
+}
+
+export default index
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export {default as Home} from './Home'
+export {default as NotFound} from './NotFound'
+export {default as Result} from './Result'
+export {default as Schedule} from './Schedule'
+export {default as ScoreBoard} from './ScoreBoard'
+export {default as Stage} from './Stage'
